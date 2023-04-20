4428004553221f98b0314118cd36cb8ef145a0c9
vanina123 <nanavanina834@gmail>
2023-04-20 12:25:20 +0100

    add file score.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './style.css';
+import Game from './score.js';
+
+const newGame = new Game();
+
+const scoreContainer = document.querySelector('.list');
+const refresh = document.querySelector('.refresh');
+const submit = document.querySelector('.submit');
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+
+const showScores = async () => {
+  const data = await newGame.AllScore();
+  for (let i = 0; i < data.length; i += 1) {
+    const scoreList = document.createElement('li');
+    scoreList.classList.add('list0');
+    scoreList.innerHTML = `${data[i].user}: ${data[i].score}`;
+    scoreContainer.appendChild(scoreList);
+  }
+};
+
+refresh.addEventListener('click', () => {
+  scoreContainer.innerHTML = '';
+  showScores();
+});
+
+submit.addEventListener('click', () => {
+  newGame.AddScore({ user: name.value, score: score.value });
+  name.value = '';
+  score.value = '';
+});
+
+window.addEventListener('load', () => {
+  showScores();
+});
